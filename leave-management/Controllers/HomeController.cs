a39ff6f277ec6408a7ce154227cb1453db26af13
Charles Pitcher <cpitcher@formulafolios.com>
2020-12-14 15:15:58 -0500

    Adds AdminLTE template

--- leave-management/Controllers/HomeController.cs
+++ leave-management/Controllers/HomeController.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using leave_management.Models;
 
 namespace leave_management.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
